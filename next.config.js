0b586451250900299593a9053d689f2d3e421f0e
Mahmud Lukman <mistamann49@gmail.com>
2023-10-20 11:53:52 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['ipfs.infura.io'],
+  },
+};
+
+module.exports = nextConfig;
