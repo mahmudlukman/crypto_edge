af77355bbe78a8b107875c4948c41a1fc95f6d51
Mahmud Lukman <mistamann49@gmail.com>
2023-10-20 17:02:31 +0100

    Navbar Completed

---
+++ components/Button.jsx
@@ -0,0 +1,14 @@
+// const primaryBtn = 'nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8 font-poppins font-semibold text-white';
+// const outlineBtn = 'border border-nft-red-violet bg-transparent font-poppins font-semibold text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8';
+
+const Button = ({ btnName, classStyles, handleClick }) => (
+  <button
+    type="button"
+    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8 font-poppins font-semibold text-white${classStyles}`}
+    onClick={handleClick}
+  >
+    {btnName}
+  </button>
+);
+
+export default Button;
