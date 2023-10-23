1263f789ff54ef3ed4d30aa848d620bc6eba198c
Mahmud Lukman <mistamann49@gmail.com>
2023-10-23 13:56:42 +0100

    Create NFT page and input field ui created

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Banner from '../components/Banner';
 import CreatorCard from '../components/CreatorCard';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
+import NFTCard from '../components/NFTCard';
 
 const Home = () => {
   const [hideButtons, setHideButtons] = useState(false);
@@ -46,72 +47,92 @@ const Home = () => {
   });
 
   return (
-    <div>
-      <h1 className="flex justify-center sm:px-4 p-12">
-        <div className="w-full minmd:w-4/5">
-          <Banner
-            name={(
-              <>
-                Discover, collect, and sell <br /> extraordinary NFTs
-              </>
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name={(
+            <>
+              Discover, collect, and sell <br /> extraordinary NFTs
+            </>
             )}
-            childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
-            parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
-          />
-          <div>
-            <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
-              Best Creators
-            </h1>
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+          parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
+            Best Creators
+          </h1>
+          <div
+            className="relative flex-1 max-w-full flex mt-3"
+            ref={parenRef}
+          >
             <div
-              className="relative flex-1 max-w-full flex mt-3"
-              ref={parenRef}
+              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              ref={scrollRef}
             >
-              <div
-                className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
-                ref={scrollRef}
-              >
-                {[6, 7, 8, 9, 10].map((i) => (
-                  <CreatorCard
-                    key={`creator-${i}`}
-                    rank={i}
-                    creatorImage={images[`creators${i}`]}
-                    creatorName={`0x${makeId(3)}...${makeId(4)}`}
-                    creatorEths={10 - i * 0.5}
+              {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i + 1}`]}
+                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
+                  creatorEths={10 - i * 0.5}
+                />
+              ))}
+              {!hideButtons && (
+              <>
+                <div
+                  onClick={() => handleScroll('left')}
+                  className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
+                >
+                  <Image
+                    src={images.left}
+                    layout="fill"
+                    objectFit="contain"
+                    alt="left_arrow"
+                    className={theme === 'light' && 'filter invert'}
                   />
-                ))}
-                {!hideButtons && (
-                  <>
-                    <div
-                      onClick={() => handleScroll('left')}
-                      className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
-                    >
-                      <Image
-                        src={images.left}
-                        layout="fill"
-                        objectFit="contain"
-                        alt="left_arrow"
-                        className={theme === 'light' && 'filter invert'}
-                      />
-                    </div>
-                    <div
-                      onClick={() => handleScroll('right')}
-                      className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
-                    >
-                      <Image
-                        src={images.right}
-                        layout="fill"
-                        objectFit="contain"
-                        alt="left_arrow"
-                        className={theme === 'light' && 'filter invert'}
-                      />
-                    </div>
-                  </>
-                )}
-              </div>
+                </div>
+                <div
+                  onClick={() => handleScroll('right')}
+                  className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
+                >
+                  <Image
+                    src={images.right}
+                    layout="fill"
+                    objectFit="contain"
+                    alt="left_arrow"
+                    className={theme === 'light' && 'filter invert'}
+                  />
+                </div>
+              </>
+              )}
             </div>
           </div>
         </div>
-      </h1>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 before:first:font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot Bids</h1>
+            <div>SearchBar</div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Cool NFT on Sale',
+                }}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/nft-details.js
+++ pages/nft-details.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-export const NFTDetails = () => {
-  return (
-    <div>NFTDetails</div>
-  )
-}
+const NFTDetails = () => <div>NFTDetails</div>;
+
+export default NFTDetails;
